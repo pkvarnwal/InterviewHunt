2d10ac91bfb8b0a29b29f4f042f72fbbe01b87b5
Pkvarnwal <pkvarnwal@gmail.com>
2021-01-27 13:45:06 +0530

    Now git commit is working

--- app/src/main/java/com/example/interviewhunt/model/SampleViewModel.kt
+++ app/src/main/java/com/example/interviewhunt/model/SampleViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
+
+
 class SampleViewModel : ViewModel() {
 
     var i: Int = 0 // Int variable declaration
